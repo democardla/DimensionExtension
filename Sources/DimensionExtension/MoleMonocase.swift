b6b0efb9d611cd72d6d6445a208cea8678124cc8
刘颖建 <democardla@icloud.com>
2024-05-07 23:19:26 +0800

    【类】摩尔单体

--- Sources/DimensionExtension/MoleMonocase.swift
+++ Sources/DimensionExtension/MoleMonocase.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+/// 摩尔单体
+public struct MoleMonocase {
+    /// 单体的摩尔质量
+    public var moleMass: Double
+
+    /// 称取的该物质的质量
+    var value: Double
+    
+    /// 计量单位
+    var unit: UnitMole
+    
+    /// 质量表达
+    public var mole: Mole {
+        return Mole(value: value, unit: unit)
+    }
+}
