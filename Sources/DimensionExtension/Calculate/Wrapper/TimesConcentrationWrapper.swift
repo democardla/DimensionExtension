bffebacc0d0a10ffe0b441c044a1ff11b0c4b01e
刘颖建 <democardla@icloud.com>
2024-05-11 01:17:27 +0800

    创建了一个倍乘浓度的包装器，可以实现相同做法的参数调用。 描述： `let a = _b ~ _c`

--- Sources/DimensionExtension/Calculate/Wrapper/TimesConcentrationWrapper.swift
+++ Sources/DimensionExtension/Calculate/Wrapper/TimesConcentrationWrapper.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+@propertyWrapper
+public struct TimesConcentrationWrapper: DataWrapperPublicHandle {
+    public typealias T = TimesConcentration
+    public init(wrappedValue: T, moleMass: Double) {
+        self.value = wrappedValue
+        self.moleMass = moleMass
+    }
+    
+    public init() {
+        moleMass = 0
+        self.value = T(value: 0, unit: .time)
+    }
+    
+    public init(value: Double) {
+        self.init()
+        self.value = T(value: value, unit: .time)
+    }
+    
+    public var wrappedValue: T {
+        get { value }
+        set { value = newValue }
+    }
+    
+    
+    public var moleMass: Double
+    public var value: T
+}
