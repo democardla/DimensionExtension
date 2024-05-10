5cc9d74503deda2bb4e08df05fd8154574f58c58
刘颖建 <democardla@icloud.com>
2024-05-11 01:20:06 +0800

    统一了自变量和应变量的类型为包装器类型 修改了换算方法

--- Sources/DimensionExtension/Calculate/Wrapper/CTCorrelation.swift
+++ Sources/DimensionExtension/Calculate/Wrapper/CTCorrelation.swift
@@ -1,32 +1,34 @@
 import Foundation
 
-
-
-
 ///``ConcentrationWrapper``与``TimesConcentration``的相关关系描述与计算方法
 public struct CTCorrelation: Correlation {
+    public typealias X = ConcentrationWrapper
+    
+    public typealias Y = TimesConcentrationWrapper
     
     /// 自变量
-    public var x: ConcentrationWrapper
-    public var y: TimesConcentration
+    public var x: X
+    public var y: Y
     
-    var coefficient: Double { return x.value.value / y.value }
+    var coefficient: Double { return x.value.value / y.value.value }
     
     public var converter: UnitConverterLinear {
         return UnitConverterLinear(coefficient: coefficient)
     }
     
-    public func x2y(_ X: ConcentrationWrapper) -> TimesConcentration {
+    public func x2y(_ X: X) -> Y {
         let value = converter.value(fromBaseUnitValue: X.value.value)
-        return TimesConcentration(value: value, unit: .time)
+        @TimesConcentrationWrapper(value: value) var res
+        return _res
     }
     
 
-    public func y2x(_ Y: TimesConcentration) -> ConcentrationWrapper {
-        let value = converter.baseUnitValue(fromValue: Y.value)
+    public func y2x(_ Y: Y) -> X {
+        let value = converter.baseUnitValue(fromValue: Y.value.value)
         @ConcentrationWrapper(moleMass: self.x.moleMass, value: value, unit: self.x.value.unit.symbol) var res
         return _res
     }
+
 }
 
 
